8f8469958103ccebd6e12bf3d0f4bd07d0d3cf45
Wamuyu <michellewachira25@gmail.com>
2022-06-16 00:40:27 +0300

    commit tree 2386bfee4632259d7cf30a9d7c4010d841da9dc6

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+    return fetch('https://anapioficeandfire.com/api/books')
+        .then(response => response.json())
+        .then(json => renderBooks(json))
 }
 
+//fetch("string representing a URL to a data source")
+
+
+
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const main = document.querySelector('main');
+    books.forEach(book => {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = book.name;
+        main.appendChild(h2);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+    fetchBooks();
 });
+//API -APPLICATION PROGRAMMING INTERFACE -->EXPOSING DATAAND OR/FUNCTIONALITY TO THE PUBLIC FOR US.ALLOWS US TO ADD IMPORTANT DATA AND FUNCTIONALITY TO THE APPS WE BUILD
+//JSON - LANGUAGE AGNOSTIC WAY OF SENDING DATA
